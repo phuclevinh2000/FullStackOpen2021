0e5c30db5decfafc502523fa3b7605164f01589b
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2021-06-15 19:32:01 +0300

    Create 2_7_App.js

---
+++ 2_7_App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import Note from './components/Book'
+
+const Header = (props) => {
+  return (
+    <h1>{props.head}</h1>
+  )
+}
+
+
+
+const App = (props) => {
+  const [ persons, setPersons] = useState([
+    { 
+      id: 1,
+      name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()    //to prevent the default
+    const noteObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+    
+    const pos = persons.find(element => element.name === newName)
+    console.log(pos)
+    if(pos) window.alert(newName + " is already added to phonebook")
+    else {
+    //create a new persons array after add
+    setPersons(persons.concat(noteObject))
+    setNewName('')}
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <Header head={"Phonebook"}/>
+      <label>name: </label>
+      <form onSubmit={addName}>
+        <input
+          value={newName}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">add</button>
+      </form>
+      <Header head={"Numbers"}/>
+      {persons.map(person => 
+          <Note key={person.id} person={person} />
+      )}
+    </div>
+  )
+}
+
+export default App
